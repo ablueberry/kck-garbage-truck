e92032573c363a7e505d9dba97e6644bf8c488da
ablueberry <rybacka.projects@gmail.com>
2016-11-29 23:19:30 +0100

    so much trucks...

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const canvas = (function() {
 
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-	ctx.fillStyle = "black";
+		ctx.fillStyle = "black";
     ctx.fill();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "black";
@@ -44,7 +44,7 @@ const canvas = (function() {
 })();
 
 const play = (function() {
-	function truck(ctx, start, end) {
+	function road(ctx, start, end) {
 		var delay = 10;
 		canvas.circle(ctx, start.x, start.y);
 		console.log('hi')
@@ -72,30 +72,24 @@ const play = (function() {
 		}, delay);
 	}
 
+	function path(ctx, graph, path) {
+		for (var i = 0; i < path.length-2; i++) {
+			let start = {
+				x: graph.nodes[path[i]].position[0],
+				y: graph.nodes[path[i]].position[1]
+			};
+			let end = {
+				x: graph.nodes[path[i+1]].position[0],
+				y: graph.nodes[path[i+1]].position[1]
+			};
+			road(ctx, start, end);
+		}
+	}
+
 	return {
-		truck: truck
+		path: path
 	}
 }());
-function garbtruck(ctx, endx, endy, startx, starty) {
-	var loc = [startx, starty] || [ctx.canvas.width/2, ctx.canvas.height/2];
-	var delay = 10;
-	canvas.circle(ctx, loc[0], loc[1]);
-	setInterval(function(){
-	if(endx === loc[0] && endy !==loc[1]){
-		ctx.clearRect(loc[0]-11, loc[1]-11, 22, 22);
-		if(loc[1] < endy) loc[1]++;
-		else if(loc[1] > endy) loc[1]--;
-		canvas.circle(ctx, loc[0], loc[1]);
-	}
-	else if(endy === loc[1] && endx !== loc[0]){
-		ctx.clearRect(loc[0]-11, loc[1]-11, 22, 22);
-		if(loc[0] < endx) loc[0]++;
-		else if(loc[0] > endx) loc[0]--;
-		canvas.circle(ctx, loc[0], loc[1]);
-	}
-	else clearInterval();
-	}, delay);
-}
 
 const map = (function() {
 	function render(ctx, graph, root_point) {
@@ -110,80 +104,129 @@ const map = (function() {
 
 			// if wykona się tylko dla nowych, niepołączonych z innymi elementów
 			// jeśli rodzic ma ustawioną pozycję to znaczy że został narysowany
-			var pos = graph.nodes[parentId - 1].position ;
+			var pos = graph.nodes[parentId].position ;
 			if (!pos.length) {
 				// ustawiamy pozycje rodzica jeśli jej nie miał
-				graph.nodes[parentId - 1].position = root;
+				graph.nodes[parentId].position = root;
 				// rysujemy rodzica
 				canvas.rect(ctx, root[0], root[1]);
 			}
 
 			// ustalamy pozycje dziecka
-			pos = graph.nodes[parentId - 1].position;
+			pos = graph.nodes[parentId].position;
 			var dir = edge.dir_form;
 			var weigth = edge.weigth;
 			var pos_child = [pos[0], pos[1]];
-			if (dir === "n") { pos_child[1] += 50 * weigth;	}
-			if (dir === "e") { pos_child[0] += 50 * weigth; }
-			if (dir === "w") { pos_child[0] -= 50 * weigth; }
-			if (dir === "s") { pos_child[1] -= 50 * weigth;}
-			graph.nodes[childId - 1].position = pos_child;
+			if (dir === "n") { pos_child[1] += 70 * weigth;	}
+			if (dir === "e") { pos_child[0] += 70 * weigth; }
+			if (dir === "w") { pos_child[0] -= 70 * weigth; }
+			if (dir === "s") { pos_child[1] -= 70 * weigth;}
+			graph.nodes[childId].position = pos_child;
 
 			// rysujemy dziecko
 			canvas.rect(ctx, pos_child[0], pos_child[1]);
 
 			// rysujemy połączenie między nimi
-			canvas.line(ctx, pos[0]-10, pos[1]-10, pos_child[0]-10, pos_child[1]-10)
+			canvas.line(ctx, pos[0]-10, pos[1]-10, pos_child[0]-10, pos_child[1]-10);
 		}
 	}
 
-	function create(ctx, min, max) {
-
-		var graph = {
-			v_amount: 0,
-
-			landfills_a: 0,
-		};
-		// Generujemy ilosc wierzcholkow
-		graph.v_amount = Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
-
-		// Generujemy ilosc wysypisk
-		graph.landfills_a = Math.floor(Math.random() * (Math.floor(0.2 * max))) + 1;
-		var landfills_left = graph.landfills_a;
-		/*
-		 			W O R K  I N  P R O G R E S S . . .
-		*/
-	}
-
 	return {
-		render: render,
-		create: create
+		render: render
 	}
 })();
 
 (function() {
 	var graph = {
-		nodes: [
-			{id: 1, type: "landfill", edges: [], position: []},
-			{id: 2, type: "home", edges: [], position: []},
-			{id: 3, type: "home", edges: [], position: []},
-			{id: 4, type: "home", edges: [], position: []},
-			{id: 5, type: "home", edges: [], position: []},
-			{id: 6, type: "home", edges: [], position: []},
-			{id: 7, type: "home", edges: [], position: []}
-		],
+		nodes: {
+			a: {type: "home", edges: [], position: []},
+			b: {type: "home", edges: [], position: []},
+			c: {type: "home", edges: [], position: []},
+			d: {type: "home", edges: [], position: []},
+			e: {type: "home", edges: [], position: []},
+			f: {type: "home", edges: [], position: []},
+			g: {type: "home", edges: [], position: []},
+			h: {type: "home", edges: [], position: []},
+			i: {type: "home", edges: [], position: []},
+			j: {type: "home", edges: [], position: []},
+			k: {type: "home", edges: [], position: []},
+			l: {type: "home", edges: [], position: []},
+			m: {type: "home", edges: [], position: []},
+			n: {type: "home", edges: [], position: []},
+			o: {type: "home", edges: [], position: []},
+			p: {type: "home", edges: [], position: []},
+			r: {type: "home", edges: [], position: []},
+			s: {type: "home", edges: [], position: []},
+			t: {type: "home", edges: [], position: []},
+			u: {type: "home", edges: [], position: []},
+			w: {type: "landfill", edges: [], position: []},
+			x: {type: "landfill", edges: [], position: []}
+		},
 		edges: [
-			{from: 1, to: 2, dir_form: "n", weigth: 1},
-			{from: 1, to: 3, dir_form: "e", weigth: 2},
-			{from: 1, to: 4, dir_form: "w", weigth: 1},
-			{from: 1, to: 5, dir_form: "s", weigth: 1},
-			{from: 5, to: 6, dir_form: "s", weigth: 3},
-			{from: 5, to: 7, dir_form: "e", weigth: 3}
+			{from: 'a', to: 'c', dir_form: "s", weigth: 1},
+			{from: 'c', to: 'b', dir_form: "w", weigth: 1},
+			{from: 'b', to: 'f', dir_form: "s", weigth: 1},
+			{from: 'f', to: 'g', dir_form: "e", weigth: 1},
+			{from: 'f', to: 'j', dir_form: "s", weigth: 1},
+			{from: 'j', to: 'k', dir_form: "e", weigth: 1},
+			{from: 'g', to: 'k', dir_form: "s", weigth: 1},
+			{from: 'g', to: 'h', dir_form: "e", weigth: 1},
+			{from: 'h', to: 'i', dir_form: "e", weigth: 2},
+			{from: 'i', to: 'd', dir_form: "n", weigth: 1},
+			{from: 'd', to: 'w', dir_form: "w", weigth: 2},
+			{from: 'd', to: 'e', dir_form: "e", weigth: 1},
+			{from: 'e', to: 'n', dir_form: "s", weigth: 2},
+			{from: 'n', to: 'm', dir_form: "w", weigth: 2},
+			{from: 'm', to: 'l', dir_form: "w", weigth: 1},
+			{from: 'm', to: 'p', dir_form: "s", weigth: 1},
+			{from: 'p', to: 'o', dir_form: "w", weigth: 2},
+			{from: 'p', to: 't', dir_form: "s", weigth: 1},
+			{from: 't', to: 's', dir_form: "w", weigth: 1},
+			{from: 's', to: 'r', dir_form: "w", weigth: 1},
+			{from: 'r', to: 'o', dir_form: "n", weigth: 1},
+			{from: 't', to: 'u', dir_form: "s", weigth: 2},
+			{from: 'u', to: 'x', dir_form: "e", weigth: 2}
 		]
 	}
+
+	var djikstra_map = {
+		a: {c: 1},
+		b: {c: 1, f: 1},
+		c: {a: 1, b: 1},
+		d: {w: 2, i: 1, e: 1},
+		e: {d: 1, n: 2},
+		f: {b: 1, g: 1, j: 1},
+		g: {f: 1, h: 1, k: 1},
+		h: {g:1, i: 2},
+		i: {d: 1, h: 2},
+		j: {f: 1, k: 1},
+		k: {j: 1, g: 1},
+		l: {m: 1},
+		m: {l: 1, n: 2, p: 1},
+		n: {m: 2, e: 2},
+		o: {r: 1, p: 2},
+		p: {m: 1, t: 1, o: 2},
+		r: {o: 1, s: 1},
+		s: {r: 1, t: 1},
+		t: {p: 1, u: 2},
+		u: {t: 2, x: 1},
+		w: {d: 2},
+		x: {u: 1},
+	};
+
 	var ctx = canvas.init();
- 	map.render(ctx, graph);
-	play.truck(ctx, {x: graph.nodes[2].position[0], y: graph.nodes[2].position[1]}, {x: graph.nodes[0].position[0], y: graph.nodes[0].position[1]});
-	play.truck(ctx,  {x: graph.nodes[0].position[0], y: graph.nodes[0].position[1]}, {x: graph.nodes[5].position[0], y: graph.nodes[5].position[1]});
-	play.truck(ctx, {x: 180, y: 180}, {x: 20, y: 20});
+	var djikstra_graph = new Graph(djikstra_map);
+
+ 	map.render(ctx, graph, [ctx.canvas.width/2, ctx.canvas.height/2 + 200]);
+
+	// test
+	var p = djikstra_graph.findShortestPath('a', 'x');
+	play.path(ctx, graph, p);
+
+	/*
+			To do:
+			1. ogarnij play.path promisami
+			2. ogarnij wielkość kółka (zmazuje ścieżki)
+			3. nazwy domków przy nich
+	*/
 }());
